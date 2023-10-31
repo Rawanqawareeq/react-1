5c468ba01160e39b10882b0faf5faf35375abb1b
Rawanqawareeq <rawanqawareeq95@gmail.com>
2023-10-31 11:06:07 +0200

    sec project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Products from './component/products/Products'
+
+
+ function App() {
+  return (
+    <>
+    <Products/>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/products/Products.jsx
@@ -0,0 +1,37 @@
+import React,{useEffect, useState ,Component } from 'react'
+
+
+function Products() {
+    let [Products,setProducts]=useState([]);
+    const getProducts= async()=>{
+        let reponse = await fetch("https://fakestoreapi.com/products");
+        let data = await reponse.json();
+        setProducts(data);
+       
+    }
+    useEffect( ()=>{
+        getProducts();
+    },[])
+   
+  return (
+    <div className='row'>
+        {Products.map( (ele)=>{
+            return <div className='col-md-4 p-5 ' key={ele.id}>
+                <h2>{ele.title}</h2>
+              
+                <img src={ele.image} className='w-100' alt={ele.title} />
+
+
+            </div>
+
+        }
+
+        )
+    }
+
+    </div>
+   
+  )
+}
+
+export default Products
